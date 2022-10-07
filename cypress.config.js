0d02a21d3e9f414600d3c98bf19ee4c40fc0c09e
Nayana Biyani <naina.savadi@gmail.com>
2022-10-07 18:22:58 +0200

    Update configuration

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,26 @@ module.exports = defineConfig({
     viewportWidth: 1366,
   	viewportHeight: 768,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
+      const exec = require('child_process').execSync;
+        module.exports = (on) => {
+          on('before:run', async (details) => {
+          console.log('override before:run');
+          await beforeRunHook(details);
+          await exec("IF EXIST cypress\\screenshots rmdir /Q /S cypress\\screenshots")
+          await exec("IF EXIST cypress\\reports rmdir /Q /S cypress\\reports")
+        });
+        on('after:run', async () => {
+          console.log('override after:run');
+          await afterRunHook();
+        });
+      };
     },
+    "reporter": "junit",
+    "reporterOptions": {
+        "reportDir": "cypress/reports",
+        "mochaFile": "cypress/reports/reports-[hash].xml",
+        "toConsole": true
+      }
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -19,3 +19,5 @@ import './helper'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+import 'cypress-mochawesome-reporter/register';
